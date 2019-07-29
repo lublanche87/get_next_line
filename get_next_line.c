152a1d45f941527f8bf4c04b3f78438096691b5e
lublanche87 <43723219+lublanche87@users.noreply.github.com>
2019-07-30 01:12:31 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sshin <sshin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 14:26:58 by sshin             #+#    #+#             */
-/*   Updated: 2019/03/05 21:25:39 by sshin            ###   ########.fr       */
+/*   Updated: 2019/07/30 01:02:36 by sshin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,38 +23,39 @@ char		*read_line(const int fd, char **line, char *str)
 	while ((ft_strchr(buf, '\n') == NULL) && ret)
 	{
 		ret = read(fd, buf, BUFF_SIZE);
-		if (ret <= 0)
+		if (ret == 0)
 			break ;
 		buf[ret] = '\0';
 		temp = str;
 		str = ft_strjoin(str, buf);
 		free(temp);
 	}
-	if (ret <= 0)
+	if (ret == 0)
 		(*line) = NULL;
-	return ((ret >= 0) ? str : (char*)-1);
+	return (str);
 }
 
-char		*ft_new_line(char **str)
+char		*ft_new_line(const int fd, char **str)
 {
 	int		len;
 	int		i;
 	char	*ptr;
 	char	*newline;
 
-	ptr = ft_strchr(*str, '\n');
+	ptr = ft_strchr(str[fd], '\n');
 	if (ptr != NULL)
 	{
-		len = (ft_strlen(*str) - (ft_strlen(ptr)));
-		newline = ft_strsub(*str, 0, len);
-		ft_memmove(*str, *str + len + 1, (i = ft_strlen(*str + len + 1)));
-		(*str)[i] = '\0';
+		len = (ft_strlen(str[fd]) - (ft_strlen(ptr)));
+		newline = ft_strsub(str[fd], 0, len);
+		i = ft_strlen(str[fd] + len + 1);
+		ft_memmove(str[fd], str[fd] + len + 1, i);
+		(str[fd])[i] = '\0';
 	}
 	else
 	{
-		newline = ft_strdup(*str);
-		free(*str);
-		*str = NULL;
+		newline = ft_strdup(str[fd]);
+		free(str[fd]);
+		str[fd] = NULL;
 	}
 	return (newline);
 }
@@ -64,16 +65,14 @@ int			get_next_line(const int fd, char **line)
 	static char		*str[FD];
 	char			buf[BUFF_SIZE + 1];
 
-	if (fd < 0 || line == NULL || read(fd, buf, 0) < 0 || fd >= FD)
+	if (fd < 0 || line == NULL || read(fd, buf, 0) < 0)
 		return (-1);
 	str[fd] = read_line(fd, line, str[fd]);
-	if (str[fd] < 0)
-		return (-1);
 	if (str[fd] && *(str[fd]) != '\0')
 	{
-		*line = ft_new_line(&(str[fd]));
+		*line = ft_new_line(fd, str);
 		return (1);
 	}
-	*line = NULL;
+	(*line) = NULL;
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,23 +6,18 @@
 /*   By: sshin <sshin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 18:23:41 by sshin             #+#    #+#             */
-/*   Updated: 2019/03/05 21:25:43 by sshin            ###   ########.fr       */
+/*   Updated: 2019/07/30 01:00:22 by sshin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 1000
-# define FD 4864
-
+# define BUFF_SIZE 100
+# define FD 65535
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
 # include "libft/libft.h"
 
 int	get_next_line(const int fd, char **line);
